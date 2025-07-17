26dc39ca6a99ba2d1faa023fa916a9af1b7b23a6
Wiktor Skrzynecki <skrzynecki.dev@gmail.com>
2025-07-17 13:30:45 +0200

    Add random quote api route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from utils.model import *
+from utils.model import get_random_quote, get_unique_categories
 
 app = Flask(__name__)
 
@@ -7,11 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', page='home')
 
+@app.route('/api/random-quote')
+def random_quote():
+    quote_data = get_random_quote()
+    return jsonify(quote_data)
+
 if __name__ == '__main__':
     categories = get_unique_categories()
-    print(f"\n✅ Found {len(categories)} unique categories:")
-
-    result = get_random_quote()
-    print(f'"{result["quote"]}"\n— {result["author"]} (Categories: {", ".join(result["categories"])})')
+    print(f"\n\nFound {len(categories)} unique categories\n\n")
+    # result = get_random_quote()
+    # print(f'\n\n"{result["quote"]}"\n— {result["author"]} (Categories: {", ".join(result["categories"])})\n\n')
 
     app.run(debug=True)
